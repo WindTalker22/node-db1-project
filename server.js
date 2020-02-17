1ae036f60c970373fae8ff39e82183a60fab101f
MooseMan <ssaciolo@gmail.com>
2020-02-17 17:35:21 -0500

    Pushing MVP Working On Stretch

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express")
 
-const db = require('./data/dbConfig.js');
+const AccountsRouter = require("./accounts/accounts-router.js")
 
-const server = express();
+const server = express()
 
-server.use(express.json());
+server.use(express.json())
 
-module.exports = server;
+server.use("/api/accounts", AccountsRouter)
+
+server.get("/", (req, res) => {
+  res.send("<h3>DB Helpers with knex</h3>")
+})
+
+module.exports = server
